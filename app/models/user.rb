779327640f1ad4596a702da78e8c07b5fd5d9017
Vivian Ha <vh8154@princeton.edu>
2024-07-25 16:41:41 -0400

    Restrict role to be user or admin Add Admin text to dashboard if user is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ require 'bcrypt'
 class User < ApplicationRecord
   has_many :appointments, dependent: :destroy
 
-  before_save :normalize_email
-  has_secure_password
+    before_save :normalize_email
+    has_secure_password
+    
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
 
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+    # makes sure role can only be admin or user
+    enum role: { admin: 'admin', user: 'user' }
+    validates :role, inclusion: { in: roles.keys }
 
   private
 
